cdf35a0927841956b2cc6e289ca6afcf6bfa3424
jshuang <1173240618@qq.com>
2021-07-19 18:26:02 +0800

    修改

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // 方法三
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.render(
     <BrowserRouter>
