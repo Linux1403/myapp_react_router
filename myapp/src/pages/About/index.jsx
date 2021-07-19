849eda29977e5944d66482ecc21285429b335837
jshuang <1173240618@qq.com>
2021-07-19 18:30:56 +0800

    update

--- myapp/src/pages/About/index.jsx
+++ myapp/src/pages/About/index.jsx
@@ -4,7 +4,7 @@ export default class About extends Component {
     render(){
         return (
             <div>
-                <h2>我是关于内容</h2>
+                <h2>我是关于内容dsadada</h2>
             </div>
         )
     }
